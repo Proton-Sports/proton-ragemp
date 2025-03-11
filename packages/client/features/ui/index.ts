4af8109d91117b004b16d2e357bae2b5a417698c
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-03-11 12:45:13 +0700

    wip

--- packages/client/features/ui/index.ts
+++ packages/client/features/ui/index.ts
@@ -6,7 +6,13 @@ export type Ui = BrowserMp & {
     focus(toggle: boolean): void;
     mount(route: string): void;
     unmount(route: string): void;
+    router: UiRouter;
 };
+
+export interface UiRouter {
+    onMount(route: string, handler: (...args: any[]) => void | (() => void)): void;
+    onDestroy(route: string, handler: (...args: any[]) => void | (() => void)): void;
+}
 
 export const createUi = (url: string): Ui => {
     const browser = mp.browsers.new(url);
@@ -39,6 +45,69 @@ export const createUi = (url: string): Ui => {
     };
 
     return Object.assign(browser, ext);
+};
+
+const createRouter = (browser: BrowserMp): UiRouter => {
+    const onMountHandlers = new Map<string, (() => void)[]>();
+    const onDestroyHandlers = new Map<string, (() => void)[]>();
+    const transientOnDestroyHandlers = new Map<string, (() => void)[]>();
+
+    const addHandler = (map: Map<string, (() => void)[]>, name: string, handler: () => void) => {
+        let handlers = map.get(name);
+        if (handlers == null) {
+            handlers = [handler];
+            onDestroyHandlers.set(name, handlers);
+        } else {
+            handlers.push(handler);
+        }
+    };
+
+    const removeHandler = (map: Map<string, (() => void)[]>, name: string, handler: () => void) => {
+        let handlers = map.get(name);
+        if (handlers == null) {
+            return;
+        }
+        const index = handlers.indexOf(handler);
+        if (index === -1) {
+            return;
+        }
+        handlers.splice(index, 1);
+    };
+
+    mp.events.add('router.mount', (route: string) => {
+        const handlers = onMountHandlers.get(route);
+        if (handlers != null) {
+            for (const handler of handlers) {
+                const ret = handler();
+                if (typeof ret === 'function') {
+                    addHandler(transientOnDestroyHandlers, route, ret);
+                }
+            }
+        }
+    });
+
+    mp.events.add('router.unmount', (route: string) => {
+        const handlers = [...(onDestroyHandlers.get(route) || []), ...(transientOnDestroyHandlers.get(route) || [])];
+        for (const handler of handlers) {
+            handler();
+        }
+        transientOnDestroyHandlers.delete(route);
+    });
+
+    return {
+        onMount: (route, handler) => {
+            addHandler(onMountHandlers, route, handler);
+            return () => {
+                removeHandler(onMountHandlers, route, handler);
+            };
+        },
+        onDestroy: (route, handler) => {
+            addHandler(onDestroyHandlers, route, handler);
+            return () => {
+                removeHandler(onDestroyHandlers, route, handler);
+            };
+        },
+    };
 };
 
 const on: Ui['on'] = (name, handler) => {
